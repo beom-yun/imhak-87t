497ed86af0f0cbbc7c6732e1249714cf0f26c4d6
beom-yun <scds0326@naver.com>
2023-04-01 20:07:56 +0900

    mainWidget, pdr.py

---
+++ mainWidget.py
@@ -0,0 +1,92 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from pdr import PDR
+
+form_class = uic.loadUiType("form.ui")[0]
+
+
+class MainWidget(QWidget, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.btn_cal.clicked.connect(self.btn_cal_clicked)
+        self.pdr = PDR()
+
+    def btn_cal_clicked(self):
+        # PDR 리셋
+        self.pdr.reset_pdr()
+
+        # UI로부터 세팅값 받아오기 & PDR 세팅값 설정
+        settings = self.get_settings()
+        self.pdr.set_pdr(**settings)
+
+        # factor1~3 계산하기
+        self.pdr.cal_factors()
+        self.edit_factor_1.setValue(self.pdr.factor_1)
+        self.edit_factor_2.setValue(self.pdr.factor_2)
+        self.edit_factor_3.setValue(self.pdr.factor_3)
+
+        # Id, Ir 계산하기
+        self.pdr.cal_i_d_and_i_r()
+        self.edit_i_d.setValue(self.pdr.i_d)
+        self.edit_i_r.setValue(self.pdr.i_r)
+        print(self.pdr.get_pdr())
+
+        # 동작여부, 동작요소 계산하기
+        pickup_result = self.pdr.cal_pickup()
+        self.edit_result_1.setText(pickup_result[0])
+        self.edit_result_2.setText(pickup_result[1])
+
+        # 그래프 그리기
+        self.draw_graph(settings)
+
+    def get_settings(self):
+        if self.radio_2.isChecked():
+            winding = 2
+        elif self.radio_3.isChecked():
+            winding = 3
+        v_1 = int(self.edit_v_1.text())
+        v_2 = int(self.edit_v_2.text())
+        v_3 = int(self.edit_v_3.text())
+        ct_ratio_1 = int(self.edit_ct_ratio_1.text())
+        ct_ratio_2 = int(self.edit_ct_ratio_2.text())
+        ct_ratio_3 = int(self.edit_ct_ratio_3.text())
+        i_n = int(self.edit_i_n.text())
+        i_low = float(self.edit_i_low.text())
+        slope_1 = float(int(self.edit_slope_1.text()) / 100)
+        slope_2 = float(int(self.edit_slope_2.text()) / 100)
+        knee_point = float(self.edit_knee_point.text())
+        i_high = float(self.edit_i_high.text())
+        i_1 = float(self.edit_i_1_r.text())
+        i_2 = float(self.edit_i_2_r.text())
+        i_3 = float(self.edit_i_3_r.text())
+
+        return {
+            "winding": winding,
+            "v_1": v_1,
+            "v_2": v_2,
+            "v_3": v_3,
+            "ct_ratio_1": ct_ratio_1,
+            "ct_ratio_2": ct_ratio_2,
+            "ct_ratio_3": ct_ratio_3,
+            "i_n": i_n,
+            "i_low": i_low,
+            "slope_1": slope_1,
+            "slope_2": slope_2,
+            "knee_point": knee_point,
+            "i_high": i_high,
+            "i_1": i_1,
+            "i_2": i_2,
+            "i_3": i_3,
+        }
+
+    def draw_graph(self, settings):
+        pass
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWidget = MainWidget()
+    myWidget.show()
+    app.exec_()
--- pdr.py
+++ pdr.py
@@ -1,5 +1,6 @@
 class PDR:
     def __init__(self):
+        self.winding = 0  # 변압기 종류(2권선 or 3권선)
         self.v_1 = 0  # 변압비 전압(1권선)
         self.v_2 = 0  # 변압비 전압(2권선)
         self.v_3 = 0  # 변압비 전압(3권선)
@@ -18,8 +19,14 @@ class PDR:
         self.cross_1 = None  # Low 전류와 Slope1의 교차점
         self.cross_2 = None  # Slope1과 Slope2의 교차점
         self.cross_3 = None  # Slope2와 High 전류의 교차점
+        self.i_d = 0  # 차전류(Id)
+        self.i_r = 0  # 억제전류(Ir)
+        self.i_1 = 0  # 1차측 전류
+        self.i_2 = 0  # 2차측 전류
+        self.i_3 = 0  # 3차측 전류
 
     def set_pdr(self, **kwargs):
+        self.winding = kwargs["winding"] if "winding" in kwargs else 0
         self.v_1 = kwargs["v_1"] if "v_1" in kwargs else 0
         self.v_2 = kwargs["v_2"] if "v_2" in kwargs else 0
         self.v_3 = kwargs["v_3"] if "v_3" in kwargs else 0
@@ -36,9 +43,15 @@ class PDR:
         self.factor_2 = kwargs["factor_2"] if "factor_2" in kwargs else 0
         self.factor_3 = kwargs["factor_3"] if "factor_3" in kwargs else 0
         self.cross_1, self.cross_2, self.cross_3 = self.cal_crossing_points()
+        self.i_d = kwargs["i_d"] if "i_d" in kwargs else 0
+        self.i_r = kwargs["i_r"] if "i_r" in kwargs else 0
+        self.i_1 = kwargs["i_1"] if "i_1" in kwargs else 0
+        self.i_2 = kwargs["i_2"] if "i_2" in kwargs else 0
+        self.i_3 = kwargs["i_3"] if "i_3" in kwargs else 0
 
     def reset_pdr(self):
         self.set_pdr(
+            winding=0,
             v_1=0,
             v_2=0,
             v_3=0,
@@ -54,10 +67,16 @@ class PDR:
             factor_1=0,
             factor_2=0,
             factor_3=0,
+            i_d=0,
+            i_r=0,
+            i_1=0,
+            i_2=0,
+            i_3=0,
         )
 
     def get_pdr(self):
         return {
+            "winding": self.winding,
             "v_1": self.v_1,
             "v_2": self.v_2,
             "v_3": self.v_3,
@@ -76,41 +95,77 @@ class PDR:
             "cross_1": self.cross_1,
             "cross_2": self.cross_2,
             "cross_3": self.cross_3,
+            "i_d": self.i_d,
+            "i_r": self.i_r,
+            "i_1": self.i_1,
+            "i_2": self.i_2,
+            "i_3": self.i_3,
         }
 
     def cal_crossing_points(self):
         if self.i_low and self.slope_1:
             cross_1 = self.i_low / self.slope_1, self.i_low
         else:
-            cross_1 = None
+            cross_1 = (0, 0)
 
         if self.slope_1 and self.slope_2:
             cross_2 = self.knee_point, self.slope_1 * self.knee_point
         else:
-            cross_2 = None
+            cross_2 = (0, 0)
 
         if self.slope_2 and self.i_high:
             cross_3 = (
                 self.i_high - (self.slope_1 - self.slope_2) * self.knee_point
             ) / self.slope_2, self.i_high
         else:
-            cross_3 = None
+            cross_3 = (0, 0)
 
         return cross_1, cross_2, cross_3
 
-    def print_pdr(self):
-        print("*" * 50)
-        print(f"변압비 {self.v_1}/{self.v_2}/{self.v_3}")
-        print(
-            f"CT비 {self.ct_ratio_1}/{self.i_n}, {self.ct_ratio_2}/{self.i_n}, {self.ct_ratio_3}/{self.i_n}"
+    def cal_factors(self):
+        self.factor_1 = 1
+        self.factor_2 = (self.ct_ratio_2 / self.ct_ratio_1) / (self.v_1 / self.v_2)
+        self.factor_3 = (
+            (self.ct_ratio_3 / self.ct_ratio_1) / (self.v_1 / self.v_3)
+            if self.winding == 3
+            else 0
         )
-        print(f"I_low {self.i_low}")
-        print(f"Knee point {self.knee_point}")
-        print(f"Slope1 {self.slope_1}")
-        print(f"Slope2 {self.slope_2}")
-        print(f"I_high {self.i_high}")
-        print(f"Factor {self.factor_1}, {self.factor_2}, {self.factor_3}")
-        print("*" * 50)
+
+    def cal_i_d_and_i_r(self):
+        i_1 = self.i_1 * self.factor_1
+        i_2 = self.i_2 * self.factor_2
+        i_3 = self.i_3 * self.factor_3
+        self.i_d = abs(i_1 - i_2 - i_3) / 5
+        self.i_r = (i_1 + i_2 + i_3) / 5
+
+    def cal_pickup(self):
+        if self.cross_3 and self.i_d >= self.i_high:
+            # print(f"차전류 {self.i_d}가 순시설정값 {self.i_high}보다 높으므로 순시 동작")
+            return "동작", "순시(87INST)"
+        if self.cross_2[0] < self.i_r:
+            graph_i_d = (
+                self.slope_2 * self.i_r
+                + (self.slope_1 - self.slope_2) * self.cross_2[0]
+            )
+            if self.i_d >= graph_i_d:
+                # print(
+                #     f"차전류 {self.i_d}가 그래프 위의 점 ({self.i_r}, {graph_i_d}) 보다 높으므로 한시 동작(slope2 영역)"
+                # )
+                return "동작", "한시(87R, Slope2)"
+        if self.cross_1[0] < self.i_r <= self.cross_2[0]:
+            graph_i_d = self.slope_1 * self.i_r
+            if self.i_d >= graph_i_d:
+                # print(
+                #     f"차전류 {self.i_d}가 그래프 위의 점 ({self.i_r}, {graph_i_d}) 보다 높으므로 한시 동작(slope1 영역)"
+                # )
+                return "동작", "한시(87R, Slope1)"
+        if self.i_r <= self.cross_1[0]:
+            if self.i_d >= self.i_low:
+                # print(
+                #     f"차전류 {self.i_r}가 그래프 위의 점 ({self.i_r}, {self.i_low}) 보다 높으므로 한시 동작(i_low 영역)"
+                # )
+                return "동작", "한시(87R)"
+        return "미동작", "-"
 
 
 # pdr = PDR()
